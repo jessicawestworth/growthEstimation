565beaa141d0e4a754715bbe372d9bb1638aefcb
Gustav Delius <gustav.delius@gmail.com>
2025-09-23 08:46:16 +0100

    Change grid in R code

--- R/simulateAge.R
+++ R/simulateAge.R
@@ -17,14 +17,14 @@ getLogLik <- function(pars, surveys, Delta_l = 1, Delta_t = 0.05) {
     t_max <- max(surveys$K) + 2 # TODO: set t_max only as high as needed
     N_l <- ceiling(l_max / Delta_l) # Number of time steps
     N_t <- ceiling(t_max / Delta_t) # Number of time steps
-    l_grid <- (1:N_l - 0.5) * Delta_l # Size grid (cell centres)
-    t_grid <- (1:(N_t + 1)) * Delta_t   # Time grid (including time = 0)
+    l_grid <- (1:N_l) * Delta_l # Size grid (bin start values)
+    a_grid <- (0:N_t) * Delta_t   # Age grid (including age = 0)
 
     G <- getGreens(pars, l_max = l_max, Delta_l = Delta_l,
                    t_max = t_max, Delta_t = Delta_t)
 
     calculate_and_aggregate_likelihood(
-        surveys, G = G, a = t_grid, l = l_grid,
+        surveys, G = G, a = a_grid, l = l_grid,
         mu = pars$spawning_mu, kappa = pars$spawning_kappa,
         annuli_date = pars$annuli_date, annuli_min_age = pars$annuli_min_age)
 }
